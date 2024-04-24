8505f3a8a79c0d4fca68f06686a82281d9f259f0
WALTER SILVESTRE COAN <walter.s@univille.br>
2024-04-24 20:07:05 -0300

    alterar registros

--- projapifso2024a/src/main/java/br/univille/projapifso2024a/entity/Cliente.java
+++ projapifso2024a/src/main/java/br/univille/projapifso2024a/entity/Cliente.java
@@ -2,6 +2,10 @@ package br.univille.projapifso2024a.entity;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,6 +15,7 @@ import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,6 +25,7 @@ public class Cliente {
     @Column(length = 1000)
     private String endereco;
     @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dataNascimento;
 
     public Date getDataNascimento() {
--- projfso2024a/src/main/java/br/univille/projfso2024a/controller/ClienteController.java
+++ projfso2024a/src/main/java/br/univille/projfso2024a/controller/ClienteController.java
@@ -3,6 +3,7 @@ package br.univille.projfso2024a.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -35,5 +36,10 @@ public class ClienteController {
         return new ModelAndView("redirect:/clientes");
     }
 
+    @GetMapping("/alterar/{id}")
+    public ModelAndView alterar(@PathVariable("id") long id){
+        var cliente = service.getById(id);
+        return new ModelAndView("cliente/form", "cliente",cliente);
+    }
 
 }
