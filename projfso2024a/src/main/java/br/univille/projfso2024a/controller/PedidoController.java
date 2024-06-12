96cb1c034482a4d5a809b104ff803a991a54f779
WALTER SILVESTRE COAN <walter.s@univille.br>
2024-06-11 21:41:50 -0300

    cadastro pai filho

--- projfso2024a/src/main/java/br/univille/projfso2024a/controller/PedidoController.java
+++ projfso2024a/src/main/java/br/univille/projfso2024a/controller/PedidoController.java
@@ -1,11 +1,16 @@
 package br.univille.projfso2024a.controller;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.univille.projfso2024a.entity.ItemPedido;
+import br.univille.projfso2024a.entity.Pedido;
 import br.univille.projfso2024a.service.PedidoService;
 import br.univille.projfso2024a.service.ProdutoService;
 
@@ -23,5 +28,39 @@ public class PedidoController {
         var listaPedidos = pedidoService.getAll();
         return new ModelAndView("pedido/index","listaPedidos",listaPedidos);
     }
+    @GetMapping("/novo")
+    public ModelAndView novo(){
+        var pedido = new Pedido();
+        var novoItem = new ItemPedido();
+        var listaProdutos = produtoService.getAll();
+        HashMap<String,Object> dados = new HashMap<>();
+        dados.put("pedido",pedido);
+        dados.put("novoItem",novoItem);
+        dados.put("listaProdutos",listaProdutos);
+         
+        return new ModelAndView("pedido/form", dados);
+    }
+
+    @PostMapping(params = "incitem")
+    public ModelAndView novo(Pedido pedido,
+                            ItemPedido novoItem){
+        pedido.getColItens().add(novoItem);
+        var listaProdutos = produtoService.getAll();
+        HashMap<String,Object> dados = new HashMap<>();
+        dados.put("pedido",pedido);
+        dados.put("novoItem",novoItem);
+        dados.put("listaProdutos",listaProdutos);
+         
+        return new ModelAndView("pedido/form", dados);
+    }
+
+    @PostMapping(params = "save")
+    public ModelAndView novo(Pedido pedido){
+        
+        pedidoService.save(pedido);
+         
+        return new ModelAndView("redirect:/pedidos");
+    }
+
     
 }
